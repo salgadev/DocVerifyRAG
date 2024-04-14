2dcf8f687de8279b55ae2c847c818abfb977a965
Sandra Ashipala <sandraashipala@gmail.com>
2024-04-14 18:25:05 +0200

    Changed heading titles

--- frontend/src/components/HowItWorks.tsx
+++ frontend/src/components/HowItWorks.tsx
@@ -41,11 +41,11 @@ export const HowItWorks = () => {
       className="container text-center py-24 sm:py-32"
     >
       <h2 className="text-3xl md:text-4xl font-bold ">
-        How It{" "}
+        Fast and Accurate{" "}
         <span className="bg-gradient-to-b from-primary/60 to-primary text-transparent bg-clip-text">
-          Works{" "}
+          Document Meta Data{" "}
         </span>
-        Step-by-Step Guide
+       Verification
       </h2>
       <p className="md:w-3/4 mx-auto mt-4 mb-8 text-xl text-muted-foreground">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis
