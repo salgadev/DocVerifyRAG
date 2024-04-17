5876325f0cceaabd228022eeceb97296fc5da2e6
Carlos Salgado <csalgado@uwo.ca>
2024-04-17 12:57:51 -0400

    add api prompt template

--- backend/generate_metadata.py
+++ backend/generate_metadata.py
@@ -24,6 +24,15 @@ vectorstore = Vectara(vectara_customer_id=vectara_customer_id,
                       vectara_corpus_id=vectara_corpus_id,
                       vectara_api_key=vectara_api_key)
 
+prompt_template = """
+BimDiscipline = ['plumbing', 'network', 'heating', 'electrical', 'ventilation', 'architecture']
+
+You are a helpful assistant that understands BIM documents and engineering disciplines. Your answer should be in JSON format and only include the title, a brief one-sentence summary, and the discipline the document belongs to, distinguishing between {[d.value for d in BimDiscipline]} based on the given document."
+
+Analyze the provided document, which could be in either German or English. Extract the title, summarize it briefly in one sentence, and infer the discipline. Document:
+context="
+"""
+
 
 def ingest(file_path):
     extension = file_path.split('.')[-1]
@@ -51,21 +60,17 @@ def ingest(file_path):
         "",
     ])
     docs = text_splitter.split_documents(documents)
-    #print(docs)
 
     return docs
 
 
-    # vectara = Vectara.from_documents(docs, embedding=FakeEmbeddings(size=768))    
-    # retriever = vectara.as_retriever()
 
-    # return retriever
-
-
-def extract_metadata(docs):    
+def extract_metadata(docs):        
     # plain text     
     context = "".join(
         [doc.page_content.replace('\n\n','').replace('..','') for doc in docs])
+
+    prompt = f'{prompt_template}{context}"'
 
     # Create client
     client = openai.OpenAI(
@@ -75,16 +80,15 @@ def extract_metadata(docs):
 
     # Call the LLM with the JSON schema
     chat_completion = client.chat.completions.create(
-        model="mistralai/Mixtral-8x7B-Instruct-v0.1",
-        response_format={"type": "json_object", "schema": Metadata.model_json_schema()},
+        model="mistralai/Mixtral-8x7B-Instruct-v0.1",        
         messages=[
             {
                 "role": "system",
-                "content": f"You are a helpful assistant that understands BIM documents and engineering disciplines. Your answer should be in JSON format and only include the title, a brief one-sentence summary, and the discipline the document belongs to, distinguishing between {[d.value for d in BimDiscipline]} based on the given document."
+                "content": f"You are a helpful assistant that responsds in JSON format"                
             },
             {
                 "role": "user",
-                "content": f"Analyze the provided document, which could be in either German or English. Extract the title, summarize it briefly in one sentence, and infer the discipline. Document:\n{context}"
+                "content": prompt                                
             }
         ]
     )
